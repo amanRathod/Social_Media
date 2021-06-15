b9c172fd68e83243bf0986d2683b9e41b420d76f
Aman Rathod <as7677379564@gmail.com>
2021-06-15 21:06:50 +0530

    complete

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,4 +1,36 @@
-/* eslint-disable prettier/prettier */
+import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/header';
+import UserProfile from '../components/profile';
+
 export default function Profile() {
-    return <p>Profile</p>
+  const { username } = useParams(); // it fetch value from 'p/username'
+  const [user, setUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const [user] = await getUserByUsername(username);
+      if (user?.userId) {
+        setUser(user);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+  }, [username, history]);
+
+  console.log('user profiel', user);
+
+  return user?.username ? (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <UserProfile user={user} />
+      </div>
+    </div>
+  ) : null;
 }
